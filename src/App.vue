981f6493ac987a8d4267e0b721002946fb6884b0
Sebastian Landwehr <153498584+dword-design-honestly@users.noreply.github.com>
2024-07-25 10:55:39 +0000

    init

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <ul class="p-5">
+    <li class="mb-2">
+      <b-tooltip title="foo" variant="danger">
+        <template #target>
+          <b-button>with component tooltip</b-button>
+        </template>
+      </b-tooltip>
+    </li>
+    <li><b-button v-b-tooltip="'foo'">with directive tooltip</b-button></li>
+  </ul>
+</template>
